15d8b0d8d25695d9d4b649a0e167d29796d1bb0d
KidCoderT <Tejas75O25@gmail.com>
2020-07-15 12:16:20 +0530

    Step 7: Created the frontend of the application with all the the data rendering properly

--- app.py
+++ app.py
@@ -4,18 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200))
     completed = db.Column(db.Boolean)
 
+
 @app.route('/')
 def index():
     incomplete_todos = Todo.query.filter_by(completed=False).all()
-    return render_template("index.html", unfinished_todos=incomplete_todos)
+    completed_todos = Todo.query.filter_by(completed=True).all()
+    return render_template("index.html", unfinished_todos=incomplete_todos, finished_todos=completed_todos)
 
 @app.route('/add', methods=["POST"])
 def add():
@@ -24,24 +26,25 @@ def add():
     db.session.commit()
     return redirect(url_for("index"))
 
-@app.route('/completed/<id>', methods=["POST"])
-def completed():
+@app.route('/completed/<id>')
+def completed(id):
     todo = Todo.query.filter_by(id=int(id)).first()
     todo.completed = True
     db.session.commit()
     return redirect(url_for("index"))
 
-@app.route('/uncheck/<id>', methods=["POST"])
-def uncheck():
+@app.route('/uncheck/<id>')
+def uncheck(id):
     todo = Todo.query.filter_by(id=int(id)).first()
     todo.completed = False
     db.session.commit()
     return redirect(url_for("index"))
 
-@app.route('/delete/<id>', methods=["POST"])
-def uncheck():
+@app.route('/delete/<id>')
+def delete(id):
     todo = Todo.query.filter_by(id=int(id)).first()
     db.session.delete(todo)
+    db.session.commit()
     return redirect(url_for("index"))
 
 if __name__ == '__main__':
