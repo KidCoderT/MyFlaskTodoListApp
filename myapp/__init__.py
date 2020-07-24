200671138768416c4e92aa4dafc621ea0b6a9966
KidCoderT <Tejas75O25@gmail.com>
2020-07-24 19:55:20 +0530

    Creating the login

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get("MyFlaskTodoListApp")
 db = SQLAlchemy(app)
 
-from app import views
+from myapp import views
