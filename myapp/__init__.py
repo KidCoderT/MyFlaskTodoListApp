94f38d395f860aa48be63978fae6df2076683a61
KidCoderT <Tejas75O25@gmail.com>
2020-08-01 15:15:12 +0530

    Made the code better and added a url mapping ability.

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -2,10 +2,10 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="./templates")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get("MyFlaskTodoListApp")
 db = SQLAlchemy(app)
 
-from myapp import views
+from myapp.views import *
--- myapp/views.py
+++ myapp/views.py
@@ -2,24 +2,24 @@ from myapp import app, db
 from myapp.models import Todo, User
 from flask import render_template, request, redirect, url_for, flash, session
 
-
+# All the views
 @app.route('/')
 @app.route('/home')
 def home():
+    session['redirect_to'] = "mytodos"
     if session.get("user_name"):
         return redirect(url_for("mytodos"))
     return render_template("home.html")
-
 
 @app.route('/mytodos')
 def mytodos():
     if session.get("user_name"):
         incomplete_todos = Todo.query.filter_by(completed=False, user_id=session.get("user_id")).all()
         completed_todos = Todo.query.filter_by(completed=True, user_id=session.get("user_id")).all()
-        return render_template("todos.html", unfinished_todos=incomplete_todos, un_todos_len=len(incomplete_todos), finished_todos=completed_todos, fin_todos_len=len(completed_todos), name=session.get("user_name"))
+        return render_template("todos.html", unfinished_todos=incomplete_todos, finished_todos=completed_todos)
     flash("Please Login to continue!!", category="error")
+    session['redirect_to'] = "mytodos"
     return redirect(url_for("login"))
-
 
 @app.route('/mytodos/add', methods=["POST", "GET"])
 def add():
@@ -29,30 +29,28 @@ def add():
         db.session.commit()
         return redirect(url_for("mytodos"))
     flash("Please Login to continue!!", category="error")
+    session['redirect_to'] = "mytodos"
     return redirect(url_for("login"))
-
 
 @app.route('/mytodos/completed/<id>')
 def completed(id):
     if session.get("user_name"):
-        todo = Todo.query.filter_by(id=int(id), user_id=session.get("user_id")).first()
-        todo.completed = True
+        todo = Todo.query.filter_by(id=int(id), user_id=session.get("user_id")).first().completed = True
         db.session.commit()
         return redirect(url_for("mytodos"))
     flash("Please Login to continue!!", category="error")
+    session['redirect_to'] = "mytodos"
     return redirect(url_for("login"))
-
 
 @app.route('/mytodos/uncheck/<id>')
 def uncheck(id):
     if session.get("user_name"):
-        todo = Todo.query.filter_by(id=int(id), user_id=session.get("user_id")).first()
-        todo.completed = False
+        todo = Todo.query.filter_by(id=int(id), user_id=session.get("user_id")).first().completed = False
         db.session.commit()
         return redirect(url_for("mytodos"))
     flash("Please Login to continue!!", category="error")
+    session['redirect_to'] = "mytodos"
     return redirect(url_for("login"))
-
 
 @app.route('/mytodos/delete/<id>')
 def delete(id):
@@ -62,24 +60,26 @@ def delete(id):
         db.session.commit()
         return redirect(url_for("mytodos"))
     flash("Please Login to continue!!", category="error")
+    session['redirect_to'] = "mytodos"
     return redirect(url_for("login"))
-
 
 @app.route('/user/login', methods=["POST", "GET"])
 def login():
     if session.get("user_name"):
-        flash("You are aldready logged there is no need to login again {}".format(session.get("user_name")), category="success")
+        flash(f"You are already logged there is no need to login again {session.get('user_name')}", category="success")
         return redirect(url_for("user_profile"))
 
     if request.method == "POST":
         user = User.query.filter_by(email=request.form.get("email")).first()
         if user:
             if user.get_password(request.form.get("password")):
-                flash("You were successfully Logged in!! Welcome back {}!!".format(user.name.title()), category="success")
+                flash(f"You were successfully Logged in!! Welcome back {user.name.title()}!!", category="success")
                 session['user_id'] = user.id
                 session['user_name'] = user.name
                 session['user_image'] = user.profile_image
-                return redirect(url_for("mytodos"))
+                if session.get("redirect_to") == "user_profile":
+                    return redirect(url_for(session.get("redirect_to"), name=session.get('user_name')))
+                return redirect(url_for(session.get("redirect_to")))
             else:
                 flash("Sorry but there was some error!!", category="error")
                 return render_template("login.html")
@@ -90,64 +90,68 @@ def login():
             return render_template("login.html", email_error=email_error, email=request.form.get("email"), password_error=password_error)
     return render_template("login.html")
 
-
 @app.route('/user/signup', methods=["POST", "GET"])
 def signup():
     if session.get("user_name"):
-        flash("You are aldready a user {}. You need to loggout if you want to create a new account!!".format(session.get("user_name")), category="success")
+        flash(f"You are already a user {session.get('user_name')}. You need to logout if you want to create a new account!!", category="success")
         return redirect(url_for("mytodos"))
 
     if request.method == "POST":
 
         form_return_data = [request.form.get("username"), request.form.get("email"), request.form.get("password"), request.form.get("repeatpassword")]
-        
+
         email_list = []
-        for i in User.query.all():   
-            email_list.append(i.__dict__["email"])
+        for i in User.query.all():
+            email_list.append(i.get("email"))
 
         if request.form.get("email") not in email_list:
             if request.form.get("password") == request.form.get("repeatpassword"):
 
                 user_list = []
-                for i in User.query.all():   
-                    user_list.append(i.__dict__["name"])
-                
+                for i in User.query.all():
+                    user_list.append(i.get("name"))
+
                 if request.form.get("username") not in user_list:
                     try:
                         new_user = User(name=request.form.get("username"), email=request.form.get("email"), password="")
                         new_user.set_password(request.form.get("password"))
                         db.session.add(new_user)
                         db.session.commit()
+                        session['redirect_to'] = "home"
                         return redirect(url_for("login"))
                     except Exception as e:
                         print(e)
                         flash("some error happened while trying to create your User Model!! Try Again", category="model")
                 else:
-                    flash("your username is aldready taken!!", category="user")
+                    flash("your username is already taken!!", category="user")
             else:
                 flash("there was some error in your password!!", category="error")
                 flash("your passwords don't match!!", category="password")
-        
+
         else:
             flash("Sorry but there was some error with your email!!", category="error")
 
         return render_template("signup.html", form_data=form_return_data)
-    
-    return render_template("signup.html")
 
+    return render_template("signup.html")
 
 @app.route('/user/logout')
 def logout():
     session["user_id"] = False
     session.pop("user_name", False)
     session.pop("user_image", None)
+    session['redirect_to'] = "mytodos"
     return redirect(url_for("home"))
-
 
 @app.route('/<name>/profile')
 def user_profile(name):
     if session.get("user_name"):
-        return render_template("user_profile.html")
+        if session.get("user_name") == name:
+            return render_template("user_profile.html")
+        else:
+            flash("You cant go to others profile !!")
+            return redirect(url_for("user_profile", name=session.get("user_name")))
     flash("Please Login to continue!!", category="error")
+    session['redirect_to'] = "user_profile"
     return redirect(url_for("login"))
 
